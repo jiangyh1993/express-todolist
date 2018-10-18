1b44017a0773631ed03106d455337255782ff53c
Unknown <158840284@qq.com>
2018-10-17 23:52:41 -0500

    创建对应mongodb

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,19 @@
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb://localhost:27017/todos",
+  { useNewUrlParser: true }
+);
+
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+var Todo = mongoose.model("todo", todoSchema);
+var itemOne = Todo({ item: "buy flowers" }).save(function(err) {
+  if (err) throw err;
+  console.log("item saved");
+});
 
 var urlencodedparser = bodyParser.urlencoded({ extended: false });
 
